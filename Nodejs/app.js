41bab9b866d6c1fa25297bd69632809b6487b3ea
Santiago Stellatelli <sstellatelli@gmail.com>
2024-03-11 15:46:32 -0300

    tarea 2 node

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,22 +22,63 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+/*----------------------------------------------------*/
+app.use(session({
+  secret: 'kwyunbshrip276x63nbn',
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/estilos', estilosRouter)
-app.use('/animacion', animadoRouter)
-app.use('/hover', hoverRouter )
+app.get('/', function (req, res) {
+  var conocido = Boolean(req.session.nombre);
+  var mayor18 = req.session.edad >=18
+
+  res.render('indextarea2', {
+    h1texto: 'Iniciar sesión',
+    conocido: conocido,
+    nombre: req.session.nombre,
+    edad: req.session.edad,
+    parrafo: req.session.parrafo,
+    edadmayor18: mayor18
+  })
+})
+
+app.post('/login', function (req, res) {
+  if (req.body.nombre) {
+    req.session.nombre = req.body.nombre
+    req.session.edad = req.body.edad
+
+    if (parseInt(req.body.edad) < 18) {
+      req.session.parrafo = 'sos menor de edad, no podras acceder al contenido';
+    } else {
+      req.session.parrafo = 'sos mayor de edad, podes acceder al contenido y por eso cambió el fondo de color';
+    }
+  }
+
+  res.redirect('/')
+})
+
+
+app.get('/salir', function (req, res) {
+  req.session.destroy();
+  res.redirect('/')
+})
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/estilos', estilosRouter)
+// app.use('/animacion', animadoRouter)
+// app.use('/hover', hoverRouter )
 
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
